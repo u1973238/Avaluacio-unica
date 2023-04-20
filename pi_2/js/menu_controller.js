316c8ebf946a10ebd3486ff2ace93ae6f5824436
u1973238 <u1973238@campus.udg.edu>
2023-04-20 15:54:49 +0200

    Create menu_controller.js

---
+++ pi_2/js/menu_controller.js
@@ -0,0 +1,19 @@
+function start_game(){
+	name = prompt("User name");
+	loadpage("./html/game.html");
+}
+
+function exit (){
+	if (name != ""){
+		alert("Leaving " + name + "'s game");
+	}
+	name = "";
+	loadpage("../index.html");
+}
+
+function options(){
+	// TODO: Open options menu
+	console.log("Options menu button");
+}
+
+
